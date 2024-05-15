24c9e106d9d524eb3eca85536d00ea21bb08f4b5
LIKITH\LIKITH-PC <likithm215@gmail.com>
2024-05-15 22:33:22 +0530

    second commit

--- attendance/settings.py
+++ attendance/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'auth'
+    'auth_log'
 ]
 
 MIDDLEWARE = [
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'attendance.urls'
+ROOT_URLCONF = 'auth_log.urls'
 
 TEMPLATES = [
     {
--- auth_log/apps.py
+++ auth_log/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class AuthConfig(AppConfig):
+class Auth_logConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'auth'
+    name = 'auth_log'
